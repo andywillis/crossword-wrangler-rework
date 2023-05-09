7d37796adaee40babe310e5f6e47e9f048a1bee3
Andy Willis <awillis@fastmail.fm>
2023-05-09 01:47:44 +0100

    Added new code

--- packages/server/src/tools/batchDownload.js
+++ packages/server/src/tools/batchDownload.js
@@ -13,8 +13,8 @@ const uriData = await getUriData(uri, years);
 await writeFile(`${rootname}/data/full.json`, JSON.stringify(uriData), 'utf8');
 console.log('File written');
 
-await writeFile(`${rootname}/data/full_unpacked.json`, JSON.stringify(uriData, null, 2), 'utf8');
-console.log('Unpacked file written');
+// await writeFile(`${rootname}/data/full_unpacked.json`, JSON.stringify(uriData, null, 2), 'utf8');
+// console.log('Unpacked file written');
 
 // console.dir(uriData, { depth: null });
 // getXMLData(uriData);
